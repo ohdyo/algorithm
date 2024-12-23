82aaa04478c82f1fc0920d4db5f6892a7b3112ee
ohdyo <smart5572@naver.com>
2024-12-23 16:16:37 +0900

    Update backjun_10026

--- bfs/backjun_10026/origin_backjun_10026.py
+++ bfs/backjun_10026/origin_backjun_10026.py
@@ -1,48 +1,55 @@
 import sys
 from collections import deque
+import copy
 
-input = sys.stdin.readline()
+drow = [-1,1,0,0]
+dcol = [0,0,-1,1]
 
-
-def bfs(row, col):
-    q = deque()
-    q.append((row, col))
-    visited[row][col] = 1
-
-    while q:
-        row, col = q.popleft()
-        drow, dcol = [1, -1, 0, 0], [0, 0, 1, -1]
+def bfs(row,col,visited,graph):
+    que = deque()
+    que.append((row,col))
+    visited.add((row,col))
+    
+    while que:
+        row, col = que.popleft()
         for i in range(4):
-            nrow = row+drow[i]
-            ncol = col+dcol[i]
-            # 배열 범위 내, 색깔 같고, 방문 안한 경우
-            if 0<=nrow<n and 0<=ncol<n:
-                if not visited[nrow][ncol]:
-                    visited[nrow][ncol] = 1  # 방문 처리
-                    q.append((nrow, ncol))
+            nrow = row + drow[i]
+            ncol = col + dcol[i]
+            if 0<= nrow and nrow <n and  0<= ncol and ncol <n :
+                if (nrow,ncol) not in visited: 
+                    if graph[nrow][ncol] == graph[row][col] :
+                        que.append((nrow,ncol))
+                        visited.add((nrow,ncol))
+                        
+def checking_area(n,graph):
+    result = [0,0]
+    tmp_graph = copy.deepcopy(graph)
+    
+    visited = set()
+    for i in range(n): 
+        for j in range(n):
+            if (i,j) not in visited:
+                bfs(i,j,visited,graph)
+                result[0] += 1
+    
+    for i in range(n):
+        for j in range(n):
+            if tmp_graph[i][j] == 'G':
+                tmp_graph[i][j] = 'R'
+    other_visited = set()
+    for i in range(n):
+        for j in range(n):
+            
+            if (i,j) not in other_visited:
+                bfs(i,j,other_visited,tmp_graph)
+                result[1] += 1
+    return result
 
 
-n = int(input)
-graph = [list(input) for _ in range(n)]
-visited = [[0]*n for _ in range(n)]
-ans = [0, 0]
-# 정상인이 볼 때의 그룹 개수
+input = sys.stdin.readline
+n = int(input())
+graph = []
 for i in range(n):
-    for j in range(n):
-        if not visited[i][j]:
-            bfs(i, j)
-            ans[0] += 1
-# 적록색맹이 볼 때의 그룹 개수를 새기 위하여 초록색을 빨간색으로 바꿈
-for i in range(n):
-    for j in range(n):
-        if graph[i][j] == 'G':
-            graph[i][j] = 'R'
-# 적록색맹인이 볼 때의 그룹 개수
-visited = [[0]*n for _ in range(n)]
-for i in range(n):
-    for j in range(n):
-        if not visited[i][j]:
-            bfs(i, j)
-            ans[1] += 1
-# 출력
-print(' '.join(map(str, ans)))
+    graph.append(list(map(str, sys.stdin.readline().strip()))) 
+
+print(checking_area(n,graph))
---
+++ bfs/backjun_14888/origin_backjun_14888.py
@@ -0,0 +1,22 @@
+import sys
+from collections import deque
+
+def bfs():
+    # 1차원 배열
+    # N-1의 빈 공간에 부호를 대입시켜 최대 최소 계산
+    # 모든 경우의 수 탐색
+    # Queue()
+    
+    que = deque()
+    
+    for i in range(n):
+        graph
+    
+    return
+
+N = map(int,sys.stdin.readline())
+graph=[0 for i in range(2*N-1)]
+graph.append(list(map()))
+
+plus, minus, multi, divid = map(int,sys.stdin.readline().split())
+
