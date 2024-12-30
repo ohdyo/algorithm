70a3057ff00d81f93044c0452c54bdc3f5627887
ohdyo <smart5572@naver.com>
2024-12-30 10:37:33 +0900

    Update baekjun_24480 dir&.py

--- dfs/baekjun_24480/origin_backjun_24480.py
+++ dfs/baekjun_24480/origin_backjun_24480.py
@@ -3,41 +3,36 @@ import sys
 #입력부
 input = sys.stdin.readline
 N, M, R = map(int,input().split())
-graph=[]
-for _ in range(N):
-    graph.append(list(map(int,input().split())))
-
-visited = [] * (N+1)
+graph=[[] for _ in range(N + 1)]
 
 
-new_graph = [[] for _ in range(N + 1)]
-
-# 간선을 순회하며 그래프를 생성
-for a, b in graph:
-    new_graph[a].append(b)
-    new_graph[b].append(a)
+#인덱스별 인접 노드 정리
+for _ in range(M):
+    u, v = map(int, input().split())
+    graph[u].append(v)
+    graph[v].append(u)
 
 # 각 노드별로 연결된 노드들을 정렬
-result_graph = [sorted(neighbors) for neighbors in new_graph]
+for i in range(1, N + 1):
+    graph[i].sort(reverse=True) 
+print(graph)
 
-
-visited = []
+#방문기록 & 최종 결과는 dfs 탐색 순서이므로 set과 리스트 선언언
+visited = set()
+result = []
 
 def dfs(graph,S):
-    visited.append(S)    
-    for node in sorted(graph[S], reverse=True):
-        if node not in visited:
-            dfs(graph, node)
-                
-    return visited
+    if S in visited:
+        return 0
+    visited.add(S)
+    result.append(S)
+    for next_node in graph[S]:
+        if next_node not in visited:
+            dfs(graph, next_node)
 
-print(dfs(result_graph,1))
-        
-    
-    
-        
-            
-        
-    
-    
-    
+
+for i in range(1, N+1):
+    if i not in visited:
+        dfs(graph,R)
+
+print(result)
