4461ec491a27abcda5afba19059c29da2a9125de
ohdyo <smart5572@naver.com>
2025-01-02 11:32:16 +0900

    add comment baekjun_17471

--- bfs/baekjun_17471/origin_baekjun_17471.py
+++ bfs/baekjun_17471/origin_baekjun_17471.py
@@ -40,12 +40,19 @@ def bfs(group):
 # 조합 생성 및 결과 계산
 for i in range(1, n // 2 + 1):
     # 1부터 n까지 범위에서 길이가 i만큼 분리해서 조합
+    # ex) n=5 일때, i = 2 인 경우
+    # (1,2), (1,3) (1,4) ...
+    # n = 5 일때, i = 3 인 경우
+    # (1,2,3) (1,2,4) (1,2,5)
     comb = list(itertools.combinations(range(1, n + 1), i))
     #생성된 조합을 각각 탐색
     for combi in comb:
         # 생성된 조합을 탐색하여 combi에 담긴 노드의 리스트를 인자로 삼아 탐색후 값 반환
         sum1, len1 = bfs(combi)
         # 조합에서 제외된 노드들끼의 리스트를 인자로 사용하여 bfs인자로 사용후 값 반환
+        # ex의 첫번째 경우 (3,4,5) , (2,4,5), (2,3,5)
+        # ex의 두번째 경우 (4,5), (3,5), (3,6)
+        # combi에 들어가지 않은 조합들의 경우를 탐색해주는 코드드
         sum2, len2 = bfs([i for i in range(1, n + 1) if i not in combi])
         if len1 + len2 == n:  # 두 그룹이 모두 연결된 경우
             result = min(result, abs(sum1 - sum2))
